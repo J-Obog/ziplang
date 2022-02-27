bffbd31e0535a51282d814564cc68ef5cfa1659b
JObog <jobogbaimhe@gmail.com>
2022-02-26 20:38:58 -0500

    refactoring token classes

--- tkn.py
+++ tkn.py
@@ -7,28 +7,22 @@ class Token:
         self.__img: str = img
         self.__pos: Position = pos
 
+    def get_position(self) -> Position:
+        return self.__pos
+
     def get_type(self) -> TokenType:
         return self.__type
 
     def get_text(self) -> str:
         return self.__img
 
-    def get_columnno(self) -> int:
-        return self.__col
-
-    def get_lineno(self) -> int:
-        return self.__line
-
     def get_length(self) -> int: 
         return len(self.__img)
 
-    def get_position(self) -> Position:
-        return self.__pos
-
 
 class TokenLiteral(Token):
-    def __init__(self, type: TokenType, img: str, col: int, line: int, val: any, data: DataType):
-        super().__init__(type, img, col, line) 
+    def __init__(self, type: TokenType, img: str, pos: Position, val: any, data: DataType):
+        super().__init__(type, img, pos)
         self.__val: any = val
         self.__data: DataType = DataType
 
@@ -36,4 +30,5 @@ class TokenLiteral(Token):
         return self.__val
 
     def get_datatype(self) -> DataType:
-        return self.__data
+        return self.__data 
+
