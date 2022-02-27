ac5712c09be64dd9f83ca6a52d8b89147c436207
JObog <jobogbaimhe@gmail.com>
2022-02-26 21:08:58 -0500

    working on lexer class

--- lexer.py
+++ lexer.py
@@ -1,4 +1,20 @@
+from typing import List, Optional
+from tkn import Token
 
 class Lexer:
     def __init__(self, input: str):
         self.__txt: str = input
+        self.__idx: int = 0
+        self.__tkns: List[Token]
+
+    def __next(self) -> Optional[Token]:
+        return None
+
+    def scan(self):
+        while self.__idx < len(self.__txt):
+            tkn = self.__next()
+            
+            if tkn != None:
+                self.__tkns.append(tkn)
+
+            self.__idx += 1 
