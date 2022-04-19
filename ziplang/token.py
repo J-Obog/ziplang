e58eb39307d5f982c024bacf121703e634f057ea
JObog <jobogbaimhe@gmail.com>
2022-04-19 09:07:01 -0400

    converting token class to dataclass

--- ziplang/token.py
+++ ziplang/token.py
@@ -1,24 +1,13 @@
+from dataclasses import dataclass
 from ziplang.position import Position
 from ziplang.constants import ZL_TOKEN_MAP
 
+@dataclass
 class Token:
-    def __init__(self, type: int, img: str, pos: Position):
-        self.__type: int = type 
-        self.__img: str = img
-        self.__pos: Position = pos
-
-    def position(self) -> Position:
-        return self.__pos
-
-    def type(self) -> int:
-        return self.__type
-
-    def text(self) -> str:
-        return self.__img
-
-    def length(self) -> int: 
-        return len(self.__img)
+    type: int
+    img: str
+    pos: Position
 
     def __repr__(self) -> str:
-        return f'[{ZL_TOKEN_MAP[self.__type]}, {self.__img}]'
+        return f'[{ZL_TOKEN_MAP[self.type]}, {self.img}]'
 
